b455d6f8f63380f373945a1bb43f26c07aef9be2
Muhammad Talha Khan <mtalhak9753@gmail.com>
2025-12-01 10:53:23 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-1683336259-62c2d",
+  "appId": "1:293090042378:web:905efdb8834680ed3075f3",
+  "apiKey": "AIzaSyBwomK1gX4udZTuS8shFQBzmNXnSAcpJm0",
+  "authDomain": "studio-1683336259-62c2d.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "293090042378"
+};
